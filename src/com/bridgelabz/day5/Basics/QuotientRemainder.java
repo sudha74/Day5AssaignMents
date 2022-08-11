af35d82cf7d56903819f11914860edf14fad6fbb
sudha123 <sudhanaukala1997@gmail.com>
2022-08-11 23:06:34 +0530

    QuotientRemainder

---
+++ src/com/bridgelabz/day5/Basics/QuotientRemainder.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.day5.Basics;
+
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Values");
+        int N=scanner.nextInt();
+        int n=scanner.nextInt();
+        int quotient =N/n;
+        System.out.println("Quotient ="+ quotient);
+        int remainder=N%n;
+        System.out.println("Remainder ="+ remainder);
+
+    }
+}
