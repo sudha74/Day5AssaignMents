7868e2beffbc690d54c1f90945245e77edaa0d9c
sudha123 <sudhanaukala1997@gmail.com>
2022-08-12 13:36:40 +0530

    Two Dimensional Array

--- src/com/bridgelabz/day5/Basics/QuotientRemainder.java
+++ src/com/bridgelabz/day5/Basics/QuotientRemainder.java
@@ -6,12 +6,12 @@ public class QuotientRemainder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the Values");
-        int N=scanner.nextInt();
-        int n=scanner.nextInt();
-        int quotient =N/n;
-        System.out.println("Quotient ="+ quotient);
-        int remainder=N%n;
-        System.out.println("Remainder ="+ remainder);
+        int N = scanner.nextInt();
+        int n = scanner.nextInt();
+        int quotient = N / n;
+        System.out.println("Quotient =" + quotient);
+        int remainder = N % n;
+        System.out.println("Remainder =" + remainder);
 
     }
 }
